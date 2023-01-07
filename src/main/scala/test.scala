0a996a0ecad7e9f3a905acfcc7d8d8a7343737c4
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-07 16:36:59 +0530

    init

---
+++ src/main/scala/test.scala
@@ -0,0 +1,20 @@
+// Creating an object
+object GfG {
+
+  // Main method
+  def main(args: Array[String]) {
+
+    // Applying console with println
+    Console.println("1")
+
+    // Displays output with no trailing lines
+    print("2")
+    print("3")
+
+    // Used for a newline
+    println()
+
+    // Displays format string
+    printf("Age = %d", 24)
+  }
+}
---
+++ src/test/scala/test.sc
@@ -0,0 +1,12 @@
+// Applying console with println
+Console.println("1")
+
+// Displays output with no trailing lines
+print("2")
+print("3")
+
+// Used for a newline
+println()
+
+// Displays format string
+printf("Age = %d", 24)
